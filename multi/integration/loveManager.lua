2fad69a1ecfc5b74acb8bbf637269b2e7448a9fa
Ryan Ward <rayaman99@gmail.com>
2019-02-25 11:19:01 -0500

    small tweaks
    There are some things that are left to be done. love2d support needs some work and there is one issue that I am trying to fix with this library. Should be able to release soonish

--- multi/integration/loveManager.lua
+++ multi/integration/loveManager.lua
@@ -1,7 +1,7 @@
 --[[
 MIT License
 
-Copyright (c) 2018 Ryan Ward
+Copyright (c) 2019 Ryan Ward
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
