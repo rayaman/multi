ef151893aba422eeb611d172b0c2c47515424f20
Ryan Ward <rayaman99@gmail.com>
2020-02-09 13:17:03 -0500

    Fixed issue created with threadedFunctions where arguments weren't being passed

--- multi/init.lua
+++ multi/init.lua
@@ -1475,7 +1475,7 @@ function multi.holdFor(n,func)
 	end)
 end
 function thread:newFunction(func,holdme)
-    return function(self,...)
+    return function(...)
 		local rets, err
 		local function wait(no) 
 			if thread.isThread() and not (no) then
@@ -1740,7 +1740,7 @@ function multi.initThreads(justThreads)
 		for i=#threads,1,-1 do
 			if not threads[i].__started then
 				if coroutine.running() ~= threads[i].thread then
-					_,ret,r1,r2,r3,r4,r5,r6=coroutine.resume(threads[i].thread,t0,t1,t2,t3,t4,t5,t6)
+					_,ret,r1,r2,r3,r4,r5,r6=coroutine.resume(threads[i].thread,unpack(threads[i].startArgs))
 					CheckRets(i)
 				end
 				threads[i].__started = true
