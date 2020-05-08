087736c947c82c90341cfc61c529bc577a531f67
Ryan Ward <rayaman99@gmail.com>
2020-05-08 17:46:19 -0400

    Fixing stuff

--- test.lua
+++ test.lua
@@ -2,6 +2,19 @@ package.path="?.lua;?/init.lua;?.lua;?/?/init.lua;"..package.path
 multi,thread = require("multi"):init()
 GLOBAL,THREAD = require("multi.integration.pesudoManager"):init()
 
+multi:newThread(function()
+    while true do
+        thread.sleep(1)
+        print("hello!")
+    end
+end)
+multi:newThread(function()
+    while true do
+        thread.sleep(1)
+        print("hello!")
+        --prrint("hehe")
+    end
+end)
 
 
 multi:mainloop({print=true})
