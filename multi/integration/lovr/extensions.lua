02a54e13eab0d9b65fdd1267e6598e8b7ce46528
Ryan Ward <rayaman99@gmail.com>
2021-05-02 22:26:35 -0400

    Started to work on lovr integration

--- multi/integration/lovr/extensions.lua
+++ multi/integration/lovr/extensions.lua
@@ -21,8 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ]]
-
--- TODO make compatible with lovr
 local multi, thread = require("multi").init()
 GLOBAL = multi.integration.GLOBAL
 THREAD = multi.integration.THREAD
