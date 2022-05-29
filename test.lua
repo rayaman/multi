831abeaccffa065c9e6fa4a4923c45bb9dfb0410
= <=>
2022-05-28 23:04:51 -0400

    Testing functions

--- test.lua
+++ test.lua
@@ -2,6 +2,17 @@ package.path = "./?/init.lua;"..package.path
 multi, thread = require("multi"):init{print=true}
 GLOBAL, THREAD = require("multi.integration.threading"):init()
 
+test = thread:newFunction(function() 
+	return 1,2
+end)
 
+ref = test()
+ref.OnError(function(...)
+	print("Got Error",...)
+end)
+
+ref.OnReturn(function(...)
+	print("Got Returns",...)
+end)
 
 multi:mainloop()
