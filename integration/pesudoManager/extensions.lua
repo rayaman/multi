3ae2acbd78830f3d35dda44da2f8d17c75b7d4ab
Ryan Ward <rayaman99@gmail.com>
2022-09-29 12:13:33 -0400

    how did pseudothreading go away

---
+++ integration/pesudoManager/extensions.lua
@@ -0,0 +1,141 @@
+--[[
+MIT License
+
+Copyright (c) 2022 Ryan Ward
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+local multi, thread = require("multi"):init()
+local GLOBAL, THREAD = multi.integration.GLOBAL,multi.integration.THREAD
+
+local function stripUpValues(func)
+    local dmp = string.dump(func)
+    if setfenv then
+        return loadstring(dmp,"IsolatedThread_PesudoThreading")
+    else
+        return load(dmp,"IsolatedThread_PesudoThreading","bt")
+    end
+end
+
+function multi:newSystemThreadedQueue(name)
+	local c = {}
+	function c:push(v)
+		table.insert(self,v)
+	end
+	function c:pop()
+		return table.remove(self,1)
+	end
+	function c:peek()
+		return self[1]
+	end
+	function c:init()
+		return self
+	end
+	GLOBAL[name or "_"] = c
+	return c
+end
+function multi:newSystemThreadedTable(name)
+    local c = {}
+    function c:init()
+        return self
+    end
+    GLOBAL[name or "_"] = c
+	return c
+end
+local setfenv = setfenv
+if not setfenv then
+    if not debug then
+        multi.print("Unable to implement setfenv in lua 5.2+ the debug module is not available!")
+    else
+        setfenv = function(f, env)
+            return load(string.dump(f), nil, nil, env)
+        end
+    end
+end
+function multi:newSystemThreadedJobQueue(n)
+    local c = {}
+    c.cores = n or THREAD.getCores()*2
+    c.OnJobCompleted = multi:newConnection()
+    local jobs = {}
+    local ID=1
+    local jid = 1
+    local env = {}
+    setmetatable(env,{
+        __index = _G
+    })
+    local funcs = {}
+    function c:doToAll(func)
+        setfenv(func,env)()
+        return self
+    end
+    function c:registerFunction(name,func)
+        funcs[name] = setfenv(func,env)
+        return self
+    end
+    function c:pushJob(name,...)
+        table.insert(jobs,{name,jid,{...}})
+        jid = jid + 1
+        return jid-1
+    end
+    function c:isEmpty()
+        print(#jobs)
+        return #jobs == 0
+    end
+    local nFunc = 0
+    function c:newFunction(name,func,holup) -- This registers with the queue
+        local func = stripUpValues(func)
+        if type(name)=="function" then
+            holup = func
+            func = name
+            name = "JQ_Function_"..nFunc
+        end
+        nFunc = nFunc + 1
+        c:registerFunction(name,func)
+        return thread:newFunction(function(...)
+            local id = c:pushJob(name,...)
+            local link
+            local rets
+            link = c.OnJobCompleted(function(jid,...)
+                if id==jid then
+                    rets = {...}
+                    link:Destroy()
+                end
+            end)
+            return thread.hold(function()
+                if rets then
+                    return unpack(rets) or multi.NIL
+                end
+            end)
+        end,holup),name
+    end
+    for i=1,c.cores do
+        thread:newthread("PesudoThreadedJobQueue_"..i,function()
+            while true do
+                thread.yield()
+                if #jobs>0 then
+                    local j = table.remove(jobs,1)
+                    c.OnJobCompleted:Fire(j[2],funcs[j[1]](unpack(j[3])))
+                else
+                    thread.sleep(.05)
+                end
+            end
+        end)
+    end
+    return c
+end 
---
+++ integration/pesudoManager/threads.lua
@@ -0,0 +1,110 @@
+--[[
+MIT License
+
+Copyright (c) 2022 Ryan Ward
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+
+local function getOS()
+	if package.config:sub(1, 1) == "\\" then
+		return "windows"
+	else
+		return "unix"
+	end
+end
+
+local function INIT(thread)
+    local THREAD = {}
+    local GLOBAL = {}
+    THREAD.Priority_Core = 3
+    THREAD.Priority_High = 2
+    THREAD.Priority_Above_Normal = 1
+    THREAD.Priority_Normal = 0
+    THREAD.Priority_Below_Normal = -1
+    THREAD.Priority_Low = -2
+    THREAD.Priority_Idle = -3
+
+    function THREAD.set(name, val)
+        GLOBAL[name] = val
+    end
+
+    function THREAD.get(name)
+        return GLOBAL[name]
+    end
+
+    function THREAD.waitFor(name)
+        return thread.hold(function() return GLOBAL[name] end)
+    end
+
+    if getOS() == "windows" then
+        THREAD.__CORES = tonumber(os.getenv("NUMBER_OF_PROCESSORS"))
+    else
+        THREAD.__CORES = tonumber(io.popen("nproc --all"):read("*n"))
+    end
+
+    function THREAD.getCores()
+        return THREAD.__CORES
+    end
+
+    function THREAD.getConsole()
+        local c = {}
+        function c.print(...)
+            print(...)
+        end
+        function c.error(err)
+            error("ERROR in <"..GLOBAL["$__THREADNAME__"]..">: "..err)
+        end
+        return c
+    end
+
+    function THREAD.getThreads()
+        return {}--GLOBAL.__THREADS__
+    end
+
+    THREAD.pushStatus = thread.pushStatus
+
+    if os.getOS() == "windows" then
+        THREAD.__CORES = tonumber(os.getenv("NUMBER_OF_PROCESSORS"))
+    else
+        THREAD.__CORES = tonumber(io.popen("nproc --all"):read("*n"))
+    end
+
+    function THREAD.kill()
+        error("Thread was killed!")
+    end
+
+    function THREAD.getName()
+        return GLOBAL["$THREAD_NAME"]
+    end
+
+    function THREAD.getID()
+        return GLOBAL["$THREAD_ID"]
+    end
+
+    THREAD.sleep = thread.sleep
+
+    THREAD.hold = thread.hold
+
+    return GLOBAL, THREAD
+end
+
+return {init = function(thread)
+    return INIT(thread)
+end}
